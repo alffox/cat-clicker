c13a0d3238767d7988c0b274a811f1bd848a8bb6
alfonsocrisci <alfonso.crisci@liferay.com>
2018-01-25 14:16:04 +0100

    Full duplication

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let clicks = 0;
+let clicksLeft = 0;
+let clicksRight = 0;
 
 var catNames = ['Ashes','Tiger','Puss','Smokey','Misty','Tigger','Kitty','Oscar','Missy','Max','Ginger'];
 
@@ -13,7 +14,12 @@ $(document).ready(function() {
     $('.cat-right-name').text(randomEl(catNames));
 });
 
-$('.cat-picture').click(function() {
-  clicks = clicks +1;
-  $('.clicks').text(clicks);
+$('.cat-left').click(function() {
+  clicksLeft = clicksLeft +1;
+  $('.counter-left').text(clicksLeft);
+});
+
+$('.cat-right').click(function() {
+  clicksRight = clicksRight +1;
+  $('.counter-right').text(clicksRight);
 });

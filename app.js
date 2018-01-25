3b91c4e6061197d9358aef7436567bcdcfc64c2e
alfonsocrisci <alfonso.crisci@liferay.com>
2018-01-25 13:22:40 +0100

    Random cat names

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 let clicks = 0;
 
+var catNames = ['Ashes','Tiger','Puss','Smokey','Misty','Tigger','Kitty','Oscar','Missy','Max','Ginger'];
+
+$(document).ready(function() {
+
+    function randomEl(list) {
+        var i = Math.floor(Math.random() * list.length);
+        return list[i];
+    }
+
+    $('.cat-left-name').text(randomEl(catNames));
+    $('.cat-right-name').text(randomEl(catNames));
+});
+
 $('.cat-picture').click(function() {
   clicks = clicks +1;
   $('.clicks').text(clicks);

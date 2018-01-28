a3059ecb0425e16fa23d3100fa8f552ed351cea7
Alfonso <alfonso.crisci.it@gmail.com>
2018-01-28 14:34:25 +0100

    bug: Use meaningful JSON name for image

--- js/app.js
+++ js/app.js
@@ -1,14 +1,10 @@
 var cats = [
-{ name: 'Ashes', image: 'cat1_960x640.jpg', clicks: '0', attribution: 'Photo by <a href="https://unsplash.com/photos/w2DsS-ZAP4U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mikhail Vasilyev</a>', source: ' via <a href="https://unsplash.com/search/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>' },
+{ name: 'Ashes', imageURL: 'cat1_960x640.jpg', clicks: '0', attribution: 'Photo by <a href="https://unsplash.com/photos/w2DsS-ZAP4U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mikhail Vasilyev</a>', source: ' via <a href="https://unsplash.com/search/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>' },
 { name: 'Tiger', image: 'cat2_960x640.jpg', clicks: '0', attribution: 'Photo by <a href="https://unsplash.com/photos/w2DsS-ZAP4U?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Paul</a>', source: ' via <a href="https://unsplash.com/search/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>' }
 ];
 
 cats.forEach(function(cat) {
     $('.cat-sidebar').append('<div class="cat-icon"><img></div>')
-    $('.cat-icon:last-child > img').attr("src", 'images/'+cat.image);
+    $('.cat-icon:last-child > img').attr("src", 'images/'+cat.imageURL);
 });
 
-
-//cats.forEach(function(cat) {
-  //console.log(cat.name + cat.image + cat.clicks + cat.attribution + cat.source);
-//});

c9091268fdb86bf525c88d8dc48c315b2a3f25de
Alfonso <alfonso.crisci.it@gmail.com>
2018-01-29 22:05:12 +0100

    feat: Click shows cat in dashboard

--- js/app.js
+++ js/app.js
@@ -1,22 +1,21 @@
-var cats = [{
-        name: 'Ashes',
-        imageURL: 'cat1_960x640.jpg',
-        authorName: 'Mikhail Vasilyev',
-        authorHyperlink: '@miklevasilyev',
-        siteName: 'Unsplash',
-        siteHyperlink: 'https://unsplash.com/'
-    },
-    {
-        name: 'Tiger',
-        imageURL: 'cat2_960x640.jpg',
-        authorName: 'Paul',
-        authorHyperlink: '@paul_',
-        siteName: 'Unsplash',
-        siteHyperlink: 'https://unsplash.com/'
-    }
+var cats = [
+{
+    name: 'Ashes',
+    imageURL: 'cat1_960x640.jpg',
+    authorName: 'Mikhail Vasilyev',
+    authorHyperlink: '@miklevasilyev',
+    siteName: 'Unsplash',
+    siteHyperlink: 'https://unsplash.com/'
+},
+{
+    name: 'Tiger',
+    imageURL: 'cat2_960x640.jpg',
+    authorName: 'Paul',
+    authorHyperlink: '@paul_',
+    siteName: 'Unsplash',
+    siteHyperlink: 'https://unsplash.com/'
+}
 ];
-
-
 
 cats.forEach(function(cat) {
     $('.cat-sidebar').append('<div class="cat-icon"><img><div class="cat-info"><div class="clicks">Clicks: <span class="counter">0</span></div><div class="name"></div><div class="attribution"><span class="salutation">Photo by </span><span class="author"><a href=""></a></span><span class="source"> via <a href=""></a></span></div></div></div>');
@@ -30,7 +29,10 @@ cats.forEach(function(cat) {
 });
 
 // Solution from https://stackoverflow.com/questions/9572825/each-div-must-increments-its-counter-upon-clicking
-     $('.cat-icon').click( function() {
-        var clicks = $(this).find('.counter');
-        clicks.text( parseInt(clicks.text()) + 1 );
-    });
+$('.cat-icon').click( function(cat) {
+    var clicks = $(this).find('.counter');
+    clicks.text( parseInt(clicks.text()) + 1 );
+
+    var clickedCat = $(this).find('img').attr('src');
+    $('.cat-big').attr("src", clickedCat);
+});

af0ea222a477527b61bd930a4fe1e07c2bf8e462
Alfonso <alfonso.crisci.it@gmail.com>
2018-01-29 21:17:55 +0100

    feat: Fetch and display cat name

--- js/app.js
+++ js/app.js
@@ -17,8 +17,9 @@ var cats = [{
 ];
 
 cats.forEach(function(cat) {
-    $('.cat-sidebar').append('<div class="cat-icon"><img><div class="cat-info"><div class="clicks">Clicks: <span class="counter">0</span></div><div class="attribution"><span class="salutation">Photo by </span><span class="author"><a href=""></a></span><span class="source"> via <a href=""></a></span></div></div></div>');
+    $('.cat-sidebar').append('<div class="cat-icon"><img><div class="cat-info"><div class="clicks">Clicks: <span class="counter">0</span></div><div class="name"></div><div class="attribution"><span class="salutation">Photo by </span><span class="author"><a href=""></a></span><span class="source"> via <a href=""></a></span></div></div></div>');
     $('.cat-icon:last-child > img').attr("src", 'images/'+cat.imageURL);
+    $('.cat-icon:last-child').find('.name').text('Name: '+cat.name);
     $('.cat-icon:last-child').find('.author > a').text(cat.authorName).attr('href', cat.siteHyperlink+cat.authorHyperlink);
     $('.cat-icon:last-child').find('.source > a').text(cat.siteName).attr('href', cat.siteHyperlink);
 });

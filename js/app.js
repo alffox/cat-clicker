a0f2e63e5aa4dbad449dec00dfd291886347fd99
Alfonso <alfonso.crisci.it@gmail.com>
2018-01-29 21:32:11 +0100

    refactor: Cast HTML jQuery finder to variable

--- js/app.js
+++ js/app.js
@@ -16,12 +16,17 @@ var cats = [{
     }
 ];
 
+
+
 cats.forEach(function(cat) {
     $('.cat-sidebar').append('<div class="cat-icon"><img><div class="cat-info"><div class="clicks">Clicks: <span class="counter">0</span></div><div class="name"></div><div class="attribution"><span class="salutation">Photo by </span><span class="author"><a href=""></a></span><span class="source"> via <a href=""></a></span></div></div></div>');
+
+    var HTMLSideBarFinder = $('.cat-icon:last-child');
+
     $('.cat-icon:last-child > img').attr("src", 'images/'+cat.imageURL);
-    $('.cat-icon:last-child').find('.name').text('Name: '+cat.name);
-    $('.cat-icon:last-child').find('.author > a').text(cat.authorName).attr('href', cat.siteHyperlink+cat.authorHyperlink);
-    $('.cat-icon:last-child').find('.source > a').text(cat.siteName).attr('href', cat.siteHyperlink);
+    $(HTMLSideBarFinder).find('.name').text('Name: '+cat.name);
+    $(HTMLSideBarFinder).find('.author > a').text(cat.authorName).attr('href', cat.siteHyperlink+cat.authorHyperlink);
+    $(HTMLSideBarFinder).find('.source > a').text(cat.siteName).attr('href', cat.siteHyperlink);
 });
 
 // Solution from https://stackoverflow.com/questions/9572825/each-div-must-increments-its-counter-upon-clicking
